dbe04e9c01617effec0a6ad66af6ea13701e48f8
Nuno Silva <nunosilva800@gmail.com>
2017-04-29 00:58:36 -0300

    Add basic pages

---
+++ app/controllers/tips_controller.rb
@@ -0,0 +1,7 @@
+class TipsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :tips, only: [:index, :show]
+  resources :contributions, only: [:index, :show, :create, :destroy]
+
+  root to: 'pages#home'
 end
