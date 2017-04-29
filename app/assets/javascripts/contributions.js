0fd24c09df307d3a91a57266f33030480b868ed2
Thiago temp <thiagotemp@MacBook-Pro-de-Macbook.local>
2017-04-29 06:59:54 -0300

    ajax created

--- app/assets/javascripts/contributions.js
+++ app/assets/javascripts/contributions.js
@@ -1,2 +1,16 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function() {
+  var formType = $('.edit_contribution');
+  var radios = $('.edit_contribution input[type="radio"]');
+
+  radios.on('click', function() {
+    $.get("/tips", {q: this.value}, function(data) {
+      $('.dailyTips').html(
+        "<h2> <a href=/tips/" + data.id + ">"+ data.title + "</a></h2>" +
+        "<a href=/tips/" + data.id + "><img src='"+ data.image_url + "'></a>" +
+        "<p>"+ data.body + "</p>"
+      );
+    });
+  })
+});
