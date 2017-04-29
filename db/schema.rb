b5444b545657712d59f678da959a2a8e704bf1c5
Nuno Silva <nunosilva800@gmail.com>
2017-04-29 04:37:00 -0300

    add missing migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170429052334) do
+ActiveRecord::Schema.define(version: 20170429070104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "contributions", force: :cascade do |t|
     t.integer  "user_id"
-    t.string   "image_url"
     t.string   "container"
     t.text     "processing_results"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["user_id"], name: "index_contributions_on_user_id", using: :btree
   end
 
